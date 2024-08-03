afd9322f610dbd11794d6a3e1b80c31a841204bf
rostyslav-horytskyi <gorzer.rostyslav@gmail.com>
2024-08-03 19:27:12 +0200

    feat: add cors layer

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+const options = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+};
+
+
+app.use(cors(options));
 
 app.get("/", (req, res) => {
   res.send("welcome from home");
